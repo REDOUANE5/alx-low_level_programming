518b71bf33e10aba8b17c018ec0633eb49494a93
REDOUANE5 <bousmararedouane@gmail.com>
2023-07-03 16:45:54 -0700

    1. memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -5,13 +5,13 @@
  * @src: memory where is copied
  * *@n: number of bytes
  *
- * Return: copied memory with n byted chnged
+ * Return: copied memory with n byted changed
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	int r = 0;
-	int i = 0;
+	int i = n;
 
 	for (; r < i; r++)
 	{
